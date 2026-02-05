52355c3c79adb98f08778d04bdf529af0fa2ebdd
Sabbir Ahmad <sa9079600@gmail.com>
2026-02-05 22:28:00 +0600

    mongodb cennect

--- index.js
+++ index.js
@@ -404,7 +404,7 @@ async function run() {
 
   
     await client.db("admin").command({ ping: 1 });
-    console.log("✅ Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("✅ successfully connected to MongoDB!");
 
     app.get("/", (req, res) => {
       res.send("Micro-Task Server Running...");
